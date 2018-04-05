37f740e2eb9682da1a1efec72fc22eda5133f0d5
Rishikesh Tiwari <rishi@kanris.biz>
2018-04-05 13:56:52 +0530

    Update UserController.java

--- src/main/java/com/kanris/toh/UserController.java
+++ src/main/java/com/kanris/toh/UserController.java
@@ -13,13 +13,21 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(method=RequestMethod.POST,value="/addUser")
 	public void adduser(@RequestBody User user) {
 		userService.addUser(user);
 	}
 	
+	@CrossOrigin(origins="http://localhost:4200")
 	@RequestMapping(method=RequestMethod.POST,value="/getUser")
 	public User getUser(@RequestBody User user) {
 		return userService.getUser(user);
+	}
+	
+	@CrossOrigin(origins="http://localhost:4200")
+	@RequestMapping(method=RequestMethod.POST,value="/updateUser")
+	public User updateUser(@RequestBody User user) {
+		return userService.updateUser(user);
 	}
 }
