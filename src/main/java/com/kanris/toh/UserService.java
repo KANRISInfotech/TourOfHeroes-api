6f675cade3d8573f059529ee434f43e49de2ae5a
Rishikesh Tiwari <rishi@kanris.biz>
2018-04-05 13:57:37 +0530

    Update UserService.java

--- src/main/java/com/kanris/toh/UserService.java
+++ src/main/java/com/kanris/toh/UserService.java
@@ -5,15 +5,29 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	public void addUser(User user) {
 		userRepository.save(user);
 	}
 
+	@SuppressWarnings("null")
 	public User getUser(User user) {
-		return userRepository.findByUserNameAndPassword(user.getUserName(), user.getPassword()); 
+		//return userRepository.findByUserNameAndPassword(user.getUserName(), user.getPassword()); 
+		User Oject_name =userRepository.findByUserNameAndPassword(user.getUserName(), user.getPassword());
+		if(Oject_name==null) {
+			User objt=new User();
+			objt.setUserName("login_failed");
+			return objt;
+		}else {
+			return Oject_name;
+		}
+
+	}
+
+	public User updateUser(User user) {
+		return userRepository.save(user); 
 	}
 }
